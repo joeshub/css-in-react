121edbdab4276f13811b85d7e42c36381e822134
joeshub <mojomail@gmail.com>
2017-01-08 15:49:43 -0800

    adding styletron

---
+++ 13-styletron/src/entry.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Styletron from 'styletron-client'
+import { StyletronProvider } from 'styletron-react'
+
+import LikeButton from './LikeButton'
+
+import '../styles/style.scss'
+
+const styleSheet = document.createElement('style')
+document.head.appendChild(styleSheet)
+const styletron = new Styletron([styleSheet])
+
+render(<StyletronProvider styletron={styletron}>
+  <LikeButton likes={9}/>
+</StyletronProvider>, document.getElementById('output'))
