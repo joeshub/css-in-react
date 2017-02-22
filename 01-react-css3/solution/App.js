0aa0524529a1b4f3db1734571432cec781dd822a
Joe Seifi <joese@trulia.com>
2017-02-21 23:33:48 -0800

    svg backgrounds

--- 01-react-css3/solution/App.js
+++ 01-react-css3/solution/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
-import '../../public/css/sxburritos.css'
+import '../../public/css/tacoshack.css'
 
 export default class App extends Component {
   render () {
     return (
       <div>
         <header className="header">
-          <h1 className="logo"><span>SXBURRITOS</span></h1>
-          <svg style={{fill:'white',width:'50px',height:'50px'}}>
-            <use xlinkHref="../../svg/foo.svg#shape" x="50" y="50" />
-          </svg>
+          <h1 className="logo"><span>Taco Shack</span></h1>
         </header>
         <section className="order">
           <main className="food">
@@ -21,34 +18,22 @@ export default class App extends Component {
                 <li className="option_item">
                   <figure className="option_item_image"><img className="option_item_photo" src="../../img/pulledpork.jpg" /></figure>
                   <div className="option_item_name">Pulled Pork<span className="option_item_price">$3.00</span></div>
-                  <button className="btn withicon depressed option_add_button">Remove</button>
+                  <button className="btn depressed option_add_button">Remove</button>
                 </li>
                 <li className="option_item">
                   <figure className="option_item_image"><img className="option_item_photo" src="../../img/guacamole.jpg" /></figure>
                   <div className="option_item_name">Guacamole<span className="option_item_price">$2.00</span></div>
-                  <button className="btn withicon option_add_button">Add
-                    <svg className="icon icon-plus">
-                      <use xlinkHref="svg/svgsprite.svg#plus"></use>
-                    </svg>
-                  </button>
+                  <button className="btn icon icon-plus option_add_button">Add</button>
                 </li>
                 <li className="option_item">
                   <figure className="option_item_image"><img className="option_item_photo" src="../../img/brownrice.jpg" /></figure>
                   <div className="option_item_name">Brown Rice<span className="option_item_price">$1.00</span></div>
-                  <button className="btn withicon option_add_button">Add
-                    <svg className="icon icon-plus">
-                      <use href="svg/svgsprite.svg#plus"></use>
-                    </svg>
-                  </button>
+                  <button className="btn icon icon-plus option_add_button">Add</button>
                 </li>
                 <li className="option_item">
                   <figure className="option_item_image"><img className="option_item_photo" src="../../img/salsa.jpg" /></figure>
                   <div className="option_item_name">Extra Salsa<span className="option_item_price">$0.50</span></div>
-                  <button className="btn withicon option_add_button">Add
-                    <svg className="icon icon-plus">
-                      <use href="svg/svgsprite.svg#plus"></use>
-                    </svg>
-                  </button>
+                  <button className="btn icon icon-plus option_add_button">Add</button>
                 </li>
               </ul>
             </div>
