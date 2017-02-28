6ba18eeb05f860d206bdffea14a4509066c71c50
Joe Seifi <joese@trulia.com>
2017-02-28 10:43:48 -0800

    use fs for copy

--- scripts/server-all.js
+++ scripts/server-all.js
@@ -1,21 +1,21 @@
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var serveStatic = require('serve-static')
-var ncp = require('ncp').ncp
-var config = require(path.join(process.cwd(), 'webpack.config'))
-var server = express()
-var compiler = webpack(config)
+const path = require('path')
+const fs = require('fs-extra')
+const express = require('express')
+const webpack = require('webpack')
+const serveStatic = require('serve-static')
+const config = require(path.join(process.cwd(), 'webpack.config'))
+const server = express()
+const compiler = webpack(config)
 
 const publicStylesPath = path.resolve(__dirname, '../public/workshop/css')
 const cssModulesPath = path.resolve(__dirname, '../05-react-css-modules/css')
 
-ncp(publicStylesPath, cssModulesPath, function (err) {
-  if(err) {
-    return console.error(err)
-  }
-  console.log('Copdied css files')
-})
+try {
+  fs.copySync(publicStylesPath, cssModulesPath)
+  console.log('CSS files copied from ' + publicStylesPath + ' to ' + cssModulesPath)
+} catch (err) {
+  console.error(err)
+}
 
 server.use(require('webpack-dev-middleware')(compiler, { 
   publicPath: config.output.publicPath,
